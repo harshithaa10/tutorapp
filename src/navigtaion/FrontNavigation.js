8ef3777849d74b9965d30ae1b8af555d5e51be07
Arun Mane <arunm@spaicing.com>
2023-04-26 09:07:38 +0530

    navugation changes

---
+++ src/navigtaion/FrontNavigation.js
@@ -0,0 +1,22 @@
+import * as React from 'react';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import SignIn from './src/screens/auth/signin';
+import SignUp from './src/screens/auth/signup';
+import ForgetPassword from './src/screens/auth/forget-password';
+
+const Stack = createNativeStackNavigator();
+
+const FrontNavigation = () => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Signin" component={SignIn} />
+        <Stack.Screen name="Signup" component={SignUp} />
+        <Stack.Screen name="ForgetPassword" component={ForgetPassword} />
+      </Stack.Navigator>
+      </NavigationContainer>
+  );
+};
+
+export default FrontNavigation;
---
+++ src/navigtaion/TutorNavigation.js
@@ -0,0 +1,18 @@
+import * as React from 'react';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import {createDrawerNavigator} from '@react-navigation/drawer';
+
+const Stack = createNativeStackNavigator();
+const Drawer = createDrawerNavigator();
+
+const TutorNavigation = () => {
+  return (
+    <Drawer.Navigator>
+      <Drawer.Screen name="Feed" component={Feed} />
+      <Drawer.Screen name="Article" component={Article} />
+    </Drawer.Navigator>
+  );
+};
+
+export default TutorNavigation;
